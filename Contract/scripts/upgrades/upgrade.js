ee2344be70b67d0fdc171848ecbe35185b240ae7
africabusinessnews <71972487+africabusinessnews@users.noreply.github.com>
2022-01-31 12:53:20 +0400

    Update upgrade.js

--- Contract/scripts/upgrades/upgrade.js
+++ Contract/scripts/upgrades/upgrade.js
@@ -4,7 +4,7 @@ const { ethers, upgrades } = require("hardhat");
 //INVENTORY 0xB8b20372bf0880359d96a3c5e51C09F670C80b87 ElfMetadataHandler
 async function main() {
   const Elves = await ethers.getContractFactory("EthernalElvesV2");
-  const elves = await upgrades.upgradeProxy("0xa351b769a01b445c04aa1b8e6275e03ec05c1e75", Elves);
+  const elves = await upgrades.upgradeProxy("0xA351B769A01B445C04AA1b8E6275e03ec05C1E75", Elves);
   console.log("upgraded");
 }
 
