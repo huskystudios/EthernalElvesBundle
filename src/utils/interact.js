c970f114341c79b757886a276cac6944da122967
africabusinessnews <71972487+africabusinessnews@users.noreply.github.com>
2022-03-27 16:03:51 +0400

    minor improvements

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -957,27 +957,6 @@ export const connectWallet = async () => {
   
   
 
-/*
-export const updateMoralisDb = async ({elf, elves}) => {
-
-  elves.set("owner_of", elf.owner.toLowerCase())
-  elves.set("action", elf.action)
-  elves.set("level", parseInt(elf.level))
-  elves.set("time", elf.time)
-  
-  elves.save()
-  .then((elf) => {
-    // Execute any logic that should take place after the object is saved.
-    console.log('Object updated with objectId: ' + elf.id );
-  }, (error) => {
-    // Execute any logic that should take place if the save fails.
-    // error is a Moralis.Error with an error code and message.
-    console.log('Failed to create new object, with error code: ' + error.message);
-  });
-  
-}
-*/
-
 /////POLYGON FUNCTIONS////
 
 
